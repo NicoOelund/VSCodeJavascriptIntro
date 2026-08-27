9c86d019ef06d00e730de78eab5641d994d2e689
LatinaEnjoyer <nioe1000@stud.ek.dk>
2026-08-27 15:00:51 +0200

    05 completed

--- DOM exercise/05-add-remove-list-items/app.js
+++ DOM exercise/05-add-remove-list-items/app.js
@@ -26,18 +26,33 @@
 
 window.addEventListener("DOMContentLoaded", initApp);
 
+let itemList = document.querySelector("#itemList");
+
 function initApp() {
 	// TODO: Add event listeners to #addBtn and #removeBtn
+	const addBtn = document.querySelector("#addBtn");
+	const removeBtn = document.querySelector("#removeBtn");
+
+	addBtn.addEventListener("click", handleAddClick);
+	removeBtn.addEventListener("click", handleRemoveClick);
 }
 
 function getNextItemNo() {
 	// TODO: return the number based one the current list size.
+	//console.log(itemList.children.length + 1);
+	return itemList.children.length + 1;
 }
 
 function handleAddClick() {
 	// TODO: Add a new <li> to #itemList, with the text "Item N" where N is the new item number
+	let li = document.createElement("li");
+	li.textContent = `Item ${getNextItemNo()}`;
+	itemList.appendChild(li);
 }
 
 function handleRemoveClick() {
 	// TODO: Remove the last <li> from #itemList
+	if (itemList.lastElementChild) {
+		itemList.lastElementChild.remove();
+	}
 }
